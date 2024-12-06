a814dfc41b13a5a2d23092409c9cb971a23bafae
Evgeniy <dev.shipitsyn@yandex.ru>
2024-12-06 17:30:23 +0300

    create country list view

--- WorldCountries/WorldCountriesList/WorldCountriesListView.swift
+++ WorldCountries/WorldCountriesList/WorldCountriesListView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct WorldCountriesListView: View {
+    @StateObject private var viewModel = WorldCountriesViewModel()
+    
     var body: some View {
         NavigationStack {
+            ZStack {
+                List {
+                    ForEach(viewModel.filteredCountries) { WorldCountriesDetailsViewModel in
+                        NavigationLink(destination: WorldCountriesDetailsView(viewModel: WorldCountriesDetailsViewModel)) {
+                            RowView(viewModel: WorldCountriesDetailsViewModel)
+                        }
+                    }
+                }
+                .navigationTitle("worldCountries")
+                .searchable(
+                    text: $viewModel.searchText
+                )
+                .disabled(viewModel.isLoading)
+                
+                if viewModel.isLoading {
+                    ProgressView()
+                        .controlSize(.large)
+                        .progressViewStyle(CircularProgressViewStyle())
+                }
+            }
+        }
+        .task {
+            await viewModel.fetchCountries()
         }
     }
 }
