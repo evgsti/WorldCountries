ea6a7db7cdb31938971d8589bdc0b4c46ea43832
Evgeniy <dev.shipitsyn@yandex.ru>
2024-12-06 17:30:32 +0300

    create country list view model

--- WorldCountries/WorldCountriesList/WorldCountriesViewModel.swift
+++ WorldCountries/WorldCountriesList/WorldCountriesViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+@MainActor
+class WorldCountriesViewModel: ObservableObject {
+    @Published var rows: [WorldCountriesDetailsViewModel] = []
+    @Published var searchText = ""
+    @Published var isLoading = false
+    
+    var filteredCountries: [WorldCountriesDetailsViewModel] {
+        if searchText.isEmpty {
+            return rows
+        } else {
+            return rows.filter { $0.countryNameCommon.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    private let networkManager = NetworkManager.shared
+    
+    func fetchCountries() async {
+        isLoading = true
+        do {
+            let countries = try await networkManager.fetchCountries()
+            rows = countries
+                .sorted { $0.name.common < $1.name.common }
+                .map { WorldCountriesDetailsViewModel(country: $0) }
+        } catch {
+            print(error)
+        }
+        isLoading = false
+    }
+}
